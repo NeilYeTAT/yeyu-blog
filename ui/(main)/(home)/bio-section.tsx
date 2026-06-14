1f01c7374168c635a0508554afee70b0ca2be598
Ye Yu <nearjilt@gmail.com>
2026-06-15 07:03:38 +0800

    chore: 🪛 update all dependencies to lastest version and refresh home bio

--- ui/(main)/(home)/bio-section.tsx
+++ ui/(main)/(home)/bio-section.tsx
@@ -4,10 +4,7 @@ export default function BioSection() {
       <h1>
         你好! 我是 <span className="font-bold text-theme-primary">叶鱼 (*´∪`)</span>
       </h1>
-      <p>
-        一位还在学习 TS 全栈开发的大四学
-        <span className="text-pink-500 line-through">生</span>牲 (≖_≖ )
-      </p>
+      <p>专注于 TypeScript 全栈开发 (ง *´▽`* )ว </p>
       <p>欢迎来到我的主页 Σ( ´･ω･`)</p>
       <p>我把此处当作我的前端试验田，看到有意思的东西都会往里面塞</p>
       <small className="text-xs md:text-sm">
